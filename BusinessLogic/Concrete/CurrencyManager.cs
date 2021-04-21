5b2cb6a7124fb3e597f27e3912c9043d5c600871
Kaan Öztürk <oztrkkaan@gmail.com>
2021-04-21 14:03:08 +0300

    Created Currency Service

---
+++ BusinessLogic/Concrete/CurrencyManager.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogic.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Concrete
+{
+   public class CurrencyManager: ICurrencyService
+    {
+
+    }
+}
